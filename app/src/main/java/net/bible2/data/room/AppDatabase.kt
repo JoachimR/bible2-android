e9be0ff10e2b3e065d0b303ac3e4e2c5e3abe33f
JoachimReiss <reiss@bryter.io>
2022-06-02 18:50:30 +0200

    fix db warning

--- app/src/main/java/net/bible2/data/room/AppDatabase.kt
+++ app/src/main/java/net/bible2/data/room/AppDatabase.kt
@@ -3,7 +3,11 @@ package net.bible2.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Entities.TheWordEntity::class, Entities.TwdEntity::class], version = 1)
+@Database(
+    entities = [Entities.TheWordEntity::class, Entities.TwdEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun twdEntity(): Entities.TwdEntity.AsDao
     abstract fun theWordEntity(): Entities.TheWordEntity.AsDao
